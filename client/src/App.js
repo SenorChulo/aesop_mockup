47848beaf12b3c6eed862949df1de56c7dc79b09
Student_M20 <student_m20@PS-DAL-M20.local>
2020-04-16 16:47:38 -0500

    added App.js App.css to have an idea on position of AromaticProducts component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import './App.css';
+import AromaticProducts from './components/AromaticProducts'
 
 function App() {
   return (
     <div className="App">
-     
-    </div>
+      <div className="page">
+        <div className="header"> 
+            HI
+        </div>
+        <div className="body"> 
+          <AromaticProducts/>
+        </div>
+
+        <div className="footer"> 
+          HI
+        </div>
+
+
+      </div>
+       </div>
   );
 }
 
