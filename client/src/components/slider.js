24d0a9e858f02aed4d5a61d82c3ed55ef048afa9
Student_M14 <student_m14@PS-DAL-M14.home>
2020-04-17 01:08:38 -0500

    add css

--- client/src/components/slider.js
+++ client/src/components/slider.js
@@ -3,10 +3,17 @@ import React from 'react';
 function Products() {
     return(
         <React.Fragment>
-            <div class = "container">
-                <ul class= "child1">omg</ul>
-                <ul class ="child2">wow</ul>
-                <ul class ="child3">swag</ul>
+            <div class = "container" id="myContainer">
+                <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+                    <a href="#">About</a>
+                    <a href="#">Services</a>
+                    <a href="#">Clients</a>
+                    <a href="#">Contact</a>
+            </div>
+
+            <span onclick="openNav()">open</span>
+            <div id="main">
+
             </div>
         </React.Fragment>
     )
